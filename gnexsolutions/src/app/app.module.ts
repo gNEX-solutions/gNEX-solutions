e7ee4fd8b61d4863944939669be78b823bff8653
Rasitha Gamage <g.rasitha@gmail.com>
2019-09-21 00:00:11 +0530

    navbar component created

--- gnexsolutions/src/app/app.module.ts
+++ gnexsolutions/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponentComponent } from './navbar-component/navbar-component.component';
+import { FooterComponentComponent } from './footer-component/footer-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponentComponent,
+    FooterComponentComponent
   ],
   imports: [
     BrowserModule,
