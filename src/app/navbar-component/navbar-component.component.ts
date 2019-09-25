72ea360bf4b0062576db2cb0c8c87c446e5ab2c7
Rasitha Gamage <g.rasitha@gmail.com>
2019-09-25 17:11:05 +0530

    nav bar finished

--- src/app/navbar-component/navbar-component.component.ts
+++ src/app/navbar-component/navbar-component.component.ts
@@ -11,7 +11,19 @@ export class NavbarComponentComponent implements OnInit {
   constructor() { }
 
   public openNav(){
-    document.getElementById("mySidenav").style.width = "250px";
+   var wid = document.getElementById("mySidenav").style.width;
+    if( wid != "250px"){
+      document.getElementById("mySidenav").style.width = "250px";
+      $(".menu-trigger-icon-1").css("transform", "translateY(6px) rotate(135deg)");
+      $(".menu-trigger-icon-2").css("transform", "translateY(-6px) rotate(-135deg)");
+      $(".menu-trigger-icon-3").css("transform", "scale(0)");
+    }
+    else{
+      this.closeNav() ;
+      $(".menu-trigger-icon-1").css("transform", "translateY(0px) rotate(0deg)");
+      $(".menu-trigger-icon-2").css("transform", "translateY(0px) rotate(0deg)");
+      $(".menu-trigger-icon-3").css("transform", "unset");
+    }
   }
 
   
