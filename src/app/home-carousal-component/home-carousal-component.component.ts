126f16b3adc2dc8fe17fcebbf7bd7e4dd50375b8
Rasitha Gamage <g.rasitha@gmail.com>
2019-09-28 15:34:37 +0530

    correction

--- src/app/home-carousal-component/home-carousal-component.component.ts
+++ src/app/home-carousal-component/home-carousal-component.component.ts
@@ -1,24 +1,29 @@
 import { Component, OnInit } from '@angular/core';
-import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
-
+import { CarouselConfig } from 'ngx-bootstrap/carousel';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-home-carousal-component',
   templateUrl: './home-carousal-component.component.html',
   styleUrls: ['./home-carousal-component.component.css'],
-  providers: [NgbCarouselConfig]  
+  providers: [{provide: CarouselConfig, useValue: { interval: 15000, noPause: true, showIndicators: true } }]  
 })
 export class HomeCarousalComponentComponent implements OnInit {
 
-
     images = [1, 2, 3].map(() => '../assets/img/home/gnex_home_image_1.jpg');
+    condition1 = true;
+    condition2 = true;
+    condition3 = true;
 
     constructor() {
 
     }
 
   ngOnInit() {
-    
-  }
+
+      setInterval(()=> {
+        // this.condition1 = false;
+      },4000); 
+    }
 
 }
