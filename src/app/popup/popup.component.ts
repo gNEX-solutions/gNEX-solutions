3956fc777cfb8a063aeed02a17b1fd7df1e8fbef
Rasitha Gamage <g.rasitha@gmail.com>
2019-10-04 14:16:05 +0530

    solutions part edited

--- src/app/popup/popup.component.ts
+++ src/app/popup/popup.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Input } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-popup',
@@ -7,9 +8,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PopupComponent implements OnInit {
 
+  @Input()
+  itaration_val: number;
+  @Input()
+  header: string;
+  @Input()
+  description: string;
+  xx:string;
   constructor() { }
 
   ngOnInit() {
-  }
+    // alert(this.itaration_val);
 
+    }
+
+    addclass(k:string){
+      // document.getElementById(k).classList.add('expand');
+      $('#'+k).toggleClass('expand');
+      document.getElementById(this.xx).setAttribute("style","z-index:1");
+    }
+
+    removeclass(k:string){
+      // alert(document.getElementById(k));
+      // document.getElementById(k).setAttribute("style","background:#ffffff");
+      // document.getElementById(k).classList.remove('expand');
+      $('#'+k).toggleClass('expand');
+      event.stopPropagation();
+      document.getElementById(this.xx).setAttribute("style","z-index:1");
+    }
+
+    circle_plus(k:string){
+      this.xx = 'circle-plus_'+k;
+      document.getElementById(this.xx).setAttribute("style","z-index:3");
+      $('#circle-plus_'+k).toggleClass('opened');
+    }
 }
